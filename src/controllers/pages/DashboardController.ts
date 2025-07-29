100f8d56d645d9f0ec5b29ba48a4d0a311f86041
Igwe Reinhard <108090171+Rigwe19@users.noreply.github.com>
2025-07-29 14:12:12 +0100

    fixed orders 404

--- src/controllers/pages/DashboardController.ts
+++ src/controllers/pages/DashboardController.ts
@@ -2,9 +2,10 @@ import { Controller, Inject } from "@tsed/di";
 import { Authenticate } from "@tsed/passport";
 import { Req } from "@tsed/platform-http";
 import { PathParams } from "@tsed/platform-params";
-import { Delete, Get, Security } from "@tsed/schema";
+import { Delete, Get, Returns, Security } from "@tsed/schema";
 import { User } from "src/models/UserModel.js";
 import { DashboardService } from "src/services/DashboardService.js";
+import { OrderService } from "src/services/OrderService.js";
 import { ProductService } from "src/services/ProductService.js";
 
 @Controller("/dashboard")
@@ -13,6 +14,8 @@ export class DashboardController {
   private readonly dashboardService: DashboardService;
   @Inject()
   private readonly productService: ProductService;
+  @Inject()
+  private readonly orderService: OrderService;
   @Authenticate("jwt", { session: false })
   @Security("jwt")
   @Get("/views")
@@ -23,34 +26,36 @@ export class DashboardController {
       views,
     };
   }
-  
-    @Get("/favourites")
-    @Authenticate("jwt", { session: false })
-    @Security("jwt")
-    async getFavourites(
-      @Req("user") user: User
-    ) {
-      const favourites = await this.productService.getUserFavourites(user._id);
-  
-      return {
-        success: true,
-        favourites: favourites
-      };
-    }
-  
-    @Delete("/favourites/:id")
-    @Authenticate("jwt", { session: false })
-    @Security("jwt")
-    async deleteFavourite(
-      @Req("user") user: User,
-      @PathParams("id") id: string
-    ) {
-      await this.productService.removeFromFavouritesById(id)
-      const favourites = await this.productService.getUserFavourites(user._id);
-  
-      return {
-        success: true,
-        favourites: favourites
-      };
-    }
+
+  @Get("/orders")
+  @Returns(200, Object)
+  async getOrders(@Req("user") user: User) {
+    const orders = await this.orderService.findAll(user._id);
+    return { success: true, message: "Success", orders };
+  }
+
+  @Get("/favourites")
+  @Authenticate("jwt", { session: false })
+  @Security("jwt")
+  async getFavourites(@Req("user") user: User) {
+    const favourites = await this.productService.getUserFavourites(user._id);
+
+    return {
+      success: true,
+      favourites: favourites,
+    };
+  }
+
+  @Delete("/favourites/:id")
+  @Authenticate("jwt", { session: false })
+  @Security("jwt")
+  async deleteFavourite(@Req("user") user: User, @PathParams("id") id: string) {
+    await this.productService.removeFromFavouritesById(id);
+    const favourites = await this.productService.getUserFavourites(user._id);
+
+    return {
+      success: true,
+      favourites: favourites,
+    };
+  }
 }
